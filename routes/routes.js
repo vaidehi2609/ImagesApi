08fb305c516390ade6433f916189df533ea94d58
vaidehibhagwat <vaidehibhagwat225@gmail.com>
2021-01-02 19:38:58 +0530

    f

--- routes/routes.js
+++ routes/routes.js
@@ -1,53 +1,53 @@
 const express = require('express')
-const path = require('path')
 const uploadImg = require('../controllers/image_upload')
 const router = express.Router()
 const ImageModel = require('../models/image_model')
 
 
+//post method for uploading images
+router.post('/upload', uploadImg.any('image'), async (req, res, next) => {
 
-router.post('/upload',uploadImg.any('image') ,async(req,res,next)=>{
-    
-   try {
-    const file = req.files
-    const files = file[0].path
-    console.log({file})
-   
-   
-    const image = new ImageModel({
-       imageUrl: `http://localhost:3000/image/${files}`
-        
-    })
-   const savedImage = await image.save()
-    res.json(savedImage)
-    console.log(savedImage)
-   } catch (error) {
-    const file = req.files
-    if(!file){
-         error = new Error('no Images')
-        error.status = 400
-        return next('error')
+    try {
+        const files = req.files[0].path
+
+        //console.log({file})
+
+
+        const image = new ImageModel({
+            imageUrl: `http://localhost:3000/${files}`
+
+        })
+        const savedImage = await image.save()
+        res.json(savedImage)
+        //console.log(savedImage)
+    } catch (error) {
+        const files = req.files
+        if (!files) {
+            error = new Error('no Images')
+            error.status = 400
+            return next('error')
+        }
+        next(error)
     }
-       next(error)
-   }
 })
 
-router.get('/imageList',async(req,res)=>{
-    ImageModel.find({},(err,images)=>{
-        if(err){
+//get method for getting all the entries in database
+router.get('/imageList', async (req, res) => {
+    ImageModel.find({}, (err, images) => {
+        if (err) {
             res.send("something went wrong");
             next();
         }
         res.json(images)
     })
 })
-router.get('/image',async(req,res)=>{
-    console.log(req.path)
-    console.log('here')
-    const Image = await ImageModel.find(req.path)
-   res.json(Image)
-   
+//router.get('/image',async(req,res)=>{
+//  console.log(req.path)
+//    console.log('here')
+//    const Image = await ImageModel.find(req.path)
+//   res.json(Image)
 
-})
+
+//})
 
 module.exports = router
